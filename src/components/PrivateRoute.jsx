cd174943620459e3a5cab5ba276982563992328f
ahmed-razic <ahmed.razic@gmail.com>
2022-02-12 23:04:31 +0100

    added privste route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,6 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthStatus } from '../hooks/useAuthStatus'
 import Spinner from './Spinner'
 
+//Route depenging of loggedin status. If User is loggedin - goes to profile route, if not - goes to sign-in route
 const PrivateRoute = () => {
   const { loggedIn, checkingStatus } = useAuthStatus()
 
